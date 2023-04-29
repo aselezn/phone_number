8141a3a78838472525a845b1d4633e85f7d12c68
as.selezneva <as.selezneva@MacBook-Pro-adm.local>
2023-04-29 21:48:09 +0300

    лаба_120_1.1 Телефоные номера

--- src/main/java/phone_number/Main.java
+++ src/main/java/phone_number/Main.java
@@ -7,7 +7,7 @@ public class Main {
 
         PhoneNumber number1 = new PhoneNumber("981", "0234567");
         PhoneNumber number2 = new PhoneNumber("7890", "0654321");
-        PhoneNumber number3 = new PhoneNumber("7890", "06543215");
+        PhoneNumber number3 = new PhoneNumber("981", "0234567");
 
         // Использование класса PhoneNumber с множествами
         HashSet<PhoneNumber> phoneSet = new HashSet<>();
@@ -21,6 +21,13 @@ public class Main {
         phoneMap.put(number2, "Person 2");
         System.out.println(phoneMap.get(number1)); // Person 1
         System.out.println(phoneMap.toString());
+
+        boolean areEqual1 = number1.equals(number2); // false, т.к. номера разные
+        boolean areEqual2 = number1.equals(number3); // true, т.к. номера одинаковые
+
+        System.out.println("number1 equals number2: " + areEqual1);
+        System.out.println("number1 equals number3: " + areEqual2);
+
 
 
     }
--- src/main/java/phone_number/PhoneNumber.java
+++ src/main/java/phone_number/PhoneNumber.java
@@ -8,20 +8,26 @@ public class PhoneNumber {
 
     // Конструктор класса PhoneNumber
     public PhoneNumber(String regionCode, String localNumber) {
+        // условие, для проверки строки - код и номер должны состоять только из цифр
+        if (!regionCode.matches("\\d+") || !localNumber.matches("\\d+")) {
+            throw new IllegalArgumentException("regionCode and localNumber must contain only digits");
+        }
         this.regionCode = regionCode;
         this.localNumber = localNumber;
     }
 
     @Override
     public String toString() {
-        String formattedLocalNumber;
-
-        if (localNumber.length() == 7) {
+        String formattedLocalNumber; // переменная, которая хранит отформатированный localNumber
+        // проверка на четное/нечетное кол-во символов в строке localNumber для верной расстановки "-"
+        if (localNumber.length() % 2 == 1) {
+            // если длина строки нечетная, то получем по индексу первые 3 симвода, добавляем дефис, далее с 4-го элемента делим символы на пары и добаляем между ними дефис
             formattedLocalNumber = localNumber.substring(0, 3) + "-" + localNumber.substring(3).replaceAll("(.{2})", "$1-");
         } else {
+            //если кол-во символов в строке - четное, делим символы на пары и добаляем между ними дефис
             formattedLocalNumber = localNumber.replaceAll("(.{2})", "$1-");
         }
-
+        // добавляем скобки для кода региона и убераем лидний дефис в конце строки
         return "(" + regionCode + ")" + formattedLocalNumber.replaceFirst("-$", "");
     }
 
